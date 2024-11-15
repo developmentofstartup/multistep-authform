a220465fa9b7fdb6e102263fa5f925f81db57253
gamar-suleymanova9 <suleymanovagamar9@gmail.com>
2024-11-15 23:35:46 +0400

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/multistep-authform/',  // Change 'repository-name' to your GitHub repo name
 })
